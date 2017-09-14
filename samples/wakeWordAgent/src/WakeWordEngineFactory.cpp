239687a75fec6c89f611edac6451523679f57d7a
soaringsoul <soaringsoul@users.noreply.github.com>
2017-09-13 21:09:08 -0400

    Update WakeWordEngineFactory.cpp

--- samples/wakeWordAgent/src/WakeWordEngineFactory.cpp
+++ samples/wakeWordAgent/src/WakeWordEngineFactory.cpp
@@ -14,7 +14,6 @@
 #include "WakeWordEngineFactory.h"
 #include "Logger.h"
 #include "WakeWordUtils.h"
-#include "KittAiSnowboyWakeWordEngine.h"
 #include "SensoryWakeWordEngine.h"
 #include "GPIOWakeWordEngine.h"
 
@@ -29,9 +28,6 @@ std::unique_ptr<WakeWordEngine> createEngine(
         EngineType engineType) {
 
   switch(engineType) {
-    case EngineType::KITT_AI_SNOWBOY_ENGINE:
-      log(Logger::DEBUG, "WakeWordEngineFactory: creating Kitt-Ai Engine");
-      return make_unique<KittAiSnowboyWakeWordEngine>(interface);
     case EngineType::SENSORY_ENGINE:
       log(Logger::DEBUG, "WakeWordEngineFactory: creating Sensory Engine");
       return make_unique<SensoryWakeWordEngine>(interface);
@@ -47,8 +43,6 @@ std::unique_ptr<WakeWordEngine> createEngine(
 std::string engineTypeToString(const EngineType type) {
 
   switch (type) {
-    case EngineType::KITT_AI_SNOWBOY_ENGINE:
-      return "KITT_AI_SNOWBOY_ENGINE";
     case EngineType::SENSORY_ENGINE:
       return "SENSORY_ENGINE";
     case EngineType::GPIO_ENGINE:
